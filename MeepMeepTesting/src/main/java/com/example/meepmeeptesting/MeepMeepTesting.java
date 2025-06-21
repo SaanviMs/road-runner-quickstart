4bf2764897371dc8d1753f64c8aefdb99b6bf805
Pranav <emmadipranav@gmail.com>
2025-06-20 21:39:44 -0700

    meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,17 +15,27 @@ public class MeepMeepTesting {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
+                .lineToX(60)
                 .turn(Math.toRadians(90))
-                .lineToY(30)
+
+                .lineToY(20)
                 .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+
+                .lineToX(-50)
+//                .turn(Math.toRadians(90))
+
+                .lineToX(-60)
+                .turn(Math.toRadians(135))
+//                .lineToY(-35)
+//                        .lineToX(60)
+//                .turn(Math.toRadians(-135))
+//                .lineToX(-50)
+//                        .lineToY(-60)
+//                .turn(Math.toRadians(135))
+
                 .build());
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_LIGHT)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
